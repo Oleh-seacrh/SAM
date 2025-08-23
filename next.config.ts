b8bfe5b911b818e7ded49e571fde14b64d65b2e9
Oleg Yunyk <oleg.yunyk@gmail.com>
2025-08-23 13:39:38 +0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false },
-  reactStrictMode: true
-};
-
-export default nextConfig;

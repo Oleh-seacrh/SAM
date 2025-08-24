2fef4dd380348a17d9e141c19a60b29bf4eab0c5
Oleg Yunyk <oleg.yunyk@gmail.com>
2025-08-24 14:59:52 +0300

    Update domain.ts

--- lib/domain.ts
+++ lib/domain.ts
@@ -3,16 +3,10 @@ export function getDomain(input: string): string {
     const u = new URL(input);
     return u.hostname.replace(/^www\./, "");
   } catch {
-    // якщо прийшов displayLink від Google
     return input.replace(/^https?:\/\//, "").replace(/^www\./, "").split("/")[0];
   }
 }
-
 export function canonicalHomepage(input: string): string {
-  try {
-    const u = new URL(input);
-    return `${u.protocol}//${u.hostname}`;
-  } catch {
-    return `https://${getDomain(input)}`;
-  }
+  try { const u = new URL(input); return `${u.protocol}//${u.hostname}`; }
+  catch { return `https://${getDomain(input)}`; }
 }
